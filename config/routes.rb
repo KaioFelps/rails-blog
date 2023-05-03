fa72485c943376c1b08a024d23c148d372ada1a5
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-05-02 23:27:15 -0300

    replaces articles routes for a resource, that brings all these routes by default

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+
+  resources :articles
   # The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
 end
